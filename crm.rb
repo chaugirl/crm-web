3c624dc5c105c2e5f266fe5d8a09897a9b642611
chaugirl <chau.marialuisa@gmail.com>
2014-10-28 14:45:44 -0400

    Display all contacts

--- crm.rb
+++ crm.rb
@@ -6,10 +6,12 @@ get '/' do  		# Welcome page always lives at '/'
 	erb :index	# index.erb file has to be in the 'views' folder in order to work
 end
 
-get '/contacts' do
+
+get "/contacts" do
+	@contacts = []
+	@contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+	@contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
+	@contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
+
 	erb :contacts
 end
-
-# get '/contacts/new' do	
-# 	erb :
-# end

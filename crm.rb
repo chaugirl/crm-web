1fe1f233d18382173bbd97e934173a21bb2f78f1
chaugirl <chau.marialuisa@gmail.com>
2014-10-28 14:33:47 -0400

    Created a new route

--- crm.rb
+++ crm.rb
@@ -4,3 +4,11 @@ get '/' do  		# Welcome page always lives at '/'
 	@crm_app_name = "My CRM"
 	erb :index	# index.erb file has to be in the 'views' folder in order to work
 end
+
+get '/contacts' do
+	erb :contacts
+end
+
+# get '/contacts/new' do	
+# 	erb :
+# end

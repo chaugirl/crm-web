1c06501db9aa4e67aaa678e12c80b832e36d5dcf
chaugirl <chau.marialuisa@gmail.com>
2014-10-30 14:38:01 -0400

    Editing a contact - added edit_contact.erb to views folder

--- crm.rb
+++ crm.rb
@@ -35,10 +35,20 @@ post '/contacts' do
 end
 
 # get '/contacts/1000' do --> finding a contact with the id 1000
-get "/contacts/:id" do #making a generalized route so we don't have to create a single route for each individual contact
+get "/contacts/:id" do 
+	#making a generalized route so we don't have to create a single route for each individual contact
 	@contact = $rolodex.find(params[:id].to_i)
 	if @contact #if contact exists
 		erb :show_contact
+	else
+		raise Sinatra::NotFound
+	end
+end
+
+get "/contacts/:id/edit" do
+	@contact = @@rolodex.find(params[:id].to_i)
+	if @contact
+		erb :edit_contact
 	else
 		raise Sinatra::NotFound
 	end
